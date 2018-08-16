bd4558316758c2d8374687cfae2063041946c89b
Krystal Foster and Talia Haller <krystal+talia@>
2018-08-16 16:51:40 -0700

    add up to point of getting nested loops to produce.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,10 @@ $(document).ready(function() {
 
   var textInput = ($("input#inputSentence").val()).toLowerCase();
   var noSpaceStr = textInput.replace(/\W/g, '');
+  var noSpaceStrArray = noSpaceStr.split('')
+  var newSentence = [];
+
+console.log(noSpaceStrArray);
 
   var sqrtInput = Math.sqrt(noSpaceStr.length);
   console.log(sqrtInput);
@@ -12,9 +16,33 @@ $(document).ready(function() {
   var rowXcolumn = Math.ceil(sqrtInput);
   console.log(rowXcolumn);
 
-  var array = noSpacesStr.split('',rowXcolumn);
-  console.log()
+loop1:
+  for (var i = 0; i < rowXcolumn; i++) {
+    newSentence += noSpaceStrArray[i];
+    console.log(newSentence);
+    // break loop1;
 
+loop2:
+    for(var j = 0; j < rowXcolumn; j+rowXcolumn-1) {
+      newSentence +=  newSentence[j];
+        newSentence.push(newSentence);
+console.log(newSentence);
+console.log(rowXcolumn);
+  break loop2;
+  }
+  }
+
+  console.log(newSentence);
+
+  // Need method that will extract characters at specified interval
+
+
+
+  newSentence = newSentence.join(" ");
+        newSentence = newSentence.toLowerCase();
+        newSentence = newSentence.replace(/^\w/, function (cap) {
+          return cap.toUpperCase();
+        });
 
 
 

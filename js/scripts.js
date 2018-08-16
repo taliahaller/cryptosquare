72df627100e2013016366d2218be98cc502ab2e8
Krystal Foster <kmfoster777@gmail.com>
2018-08-15 18:57:22 -0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
   event.preventDefault();
-  $(".result").show();
-
+  $(".well").show();
   var numberInput = $("input#plainNumber").val();
   $(".answer").text(numberInput);
 
+  function romanize(num) {
+   var romanString = '';
+   var roman = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+   var numeral = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+   for ( var i in numeral ) {
+   	while ( num >= numeral[i] ) {
+       	romanString += roman[i];
+           num -= numeral[i];
+       }
+   }
+   return romanString;
+
+}
 
   });
 });
